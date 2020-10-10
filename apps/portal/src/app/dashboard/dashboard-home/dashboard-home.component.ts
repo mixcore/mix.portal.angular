6a34aea98e41a0ab00b12d34e7fa2ab6e89f9e81
Huy Nguyen <Smilefounder@users.noreply.github.com>
2020-10-10 14:48:21 +0700

    upd

--- apps/portal/src/app/dashboard/dashboard-home/dashboard-home.component.ts
+++ apps/portal/src/app/dashboard/dashboard-home/dashboard-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
 import { NotificationService } from '../../../../src/app/core/services/notification.service';
 import { Title } from '@angular/platform-browser';
 import { NGXLogger } from 'ngx-logger';
@@ -7,9 +9,26 @@ import { AuthenticationService } from '../../../../src/app/core/services/auth.se
 @Component({
   selector: 'mixcore-dashboard-home',
   templateUrl: './dashboard-home.component.html',
-  styleUrls: ['./dashboard-home.component.css']
+  styleUrls: ['./dashboard-home.component.scss']
 })
 export class DashboardHomeComponent implements OnInit {
+
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi',
+    'Episode IX – The Rise of Skywalker'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  }
+
   currentUser: any;
 
   constructor(private notificationService: NotificationService,
