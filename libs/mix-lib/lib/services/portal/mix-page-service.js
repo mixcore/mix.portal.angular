4a19005f166559a32d86071668a5c7de1cc3922a
Hoang Nguyen Nhat <hoang.nguyen@orientsoftware.net>
2021-05-07 17:34:29 +0700

    update config

---
+++ libs/mix-lib/lib/services/portal/mix-page-service.js
@@ -0,0 +1,8 @@
+import { MixModelType } from '../../enums/mix-enums';
+import { MixRestPortalService } from '../base/mix-rest-portal-service';
+export class PageService extends MixRestPortalService {
+    constructor() {
+        super(MixModelType.Page);
+    }
+}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWl4LXBhZ2Utc2VydmljZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uLy4uLy4uL21peC5saWIudHMvc3JjL2xpYi9zZXJ2aWNlcy9wb3J0YWwvbWl4LXBhZ2Utc2VydmljZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxPQUFPLEVBQUUsWUFBWSxFQUFFLE1BQU0sdUJBQXVCLENBQUM7QUFFckQsT0FBTyxFQUFFLG9CQUFvQixFQUFFLE1BQU0saUNBQWlDLENBQUM7QUFFdkUsTUFBTSxPQUFPLFdBQVksU0FBUSxvQkFBbUM7SUFDbEU7UUFDRSxLQUFLLENBQUMsWUFBWSxDQUFDLElBQUksQ0FBQyxDQUFDO0lBQzNCLENBQUM7Q0FDRiJ9
