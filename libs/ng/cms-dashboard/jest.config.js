86550da40ce2e3cf9e866753c9badd9d6e5dab59
Restyled.io <commits@restyled.io>
2021-07-04 05:45:59 +0000

    Restyled by clang-format

--- libs/ng/cms-dashboard/jest.config.js
+++ libs/ng/cms-dashboard/jest.config.js
@@ -1,23 +1,19 @@
 module.exports = {
-  displayName: 'ng-cms-dashboard',
-  preset: '../../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$'
+  displayName : 'ng-cms-dashboard',
+  preset : '../../../jest.preset.js',
+  setupFilesAfterEnv : [ '<rootDir>/src/test-setup.ts' ],
+  globals : {
+    'ts-jest' : {
+      tsconfig : '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex : '\\.(html|svg)$'
     }
   },
-  coverageDirectory: '../../../coverage/libs/ng/cms-dashboard',
-  transform: {
-    '^.+\\.(ts|js|html)$': 'jest-preset-angular'
-  },
-  snapshotSerializers: [
+  coverageDirectory : '../../../coverage/libs/ng/cms-dashboard',
+  transform : {'^.+\\.(ts|js|html)$' : 'jest-preset-angular'},
+  snapshotSerializers : [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment'
   ],
-  moduleNameMapper: {
-    '^lodash-es$': 'lodash'
-  }
+  moduleNameMapper : {'^lodash-es$' : 'lodash'}
 };
