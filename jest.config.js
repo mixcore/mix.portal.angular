5ef347ff7d40379161c5cf6319493863ec466b41
phong3091998 <phong.cao@orientsoftware.com>
2021-08-17 11:05:14 +0700

    feat(mix-portal): init category, database, file, user structure

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,12 @@ module.exports = {
     '<rootDir>/libs/ng/cms-page',
     '<rootDir>/libs/ng/cms-module',
     '<rootDir>/libs/ng/shared',
-    '<rootDir>/libs/ng/cms-navigation'
+    '<rootDir>/libs/ng/cms-navigation',
+    '<rootDir>/libs/ng/cms-category',
+    '<rootDir>/libs/ng/cms-database',
+    '<rootDir>/libs/ng/cms-file',
+    '<rootDir>/libs/ng/cms-tag',
+    '<rootDir>/libs/ng/cms-user',
+    '<rootDir>/libs/ng/cms-theme'
   ]
 };
---
+++ libs/ng/cms-file/src/index.ts
@@ -0,0 +1,1 @@
+export * from './cms-file.module';
