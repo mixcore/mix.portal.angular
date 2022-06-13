7f5065b9137dad0d0ea9f4887623b6535ce26624
Huy Nguyen <Smilefounder@users.noreply.github.com>
2022-06-13 14:15:15 +0700

    config run with ssl

--- apps/mix-portal/src/environments/environment.ts
+++ apps/mix-portal/src/environments/environment.ts
@@ -8,8 +8,8 @@
 
 export const environment = {
   production: false,
-  domainUrl: 'https://v2.dev.mixcore.org/',
-  baseUrl: 'https://v2.dev.mixcore.org/api/v2/rest',
+  domainUrl: '//v2.dev.mixcore.org/',
+  baseUrl: '//v2.dev.mixcore.org/api/v2/rest',
   getThemeUrl:
-    'https://store.mixcore.org/api/v1/rest/en-us/post/client?pageSize=20&status=Published&orderBy=CreatedDateTime&direction=Desc&postType=theme'
+    '//store.mixcore.org/api/v1/rest/en-us/post/client?pageSize=20&status=Published&orderBy=CreatedDateTime&direction=Desc&postType=theme'
 };
