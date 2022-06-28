23aa443a1e9a0282fae28fe915d1d077930b3baf
phong cao <phongcao3091998@gmail.com>
2022-06-28 21:01:29 +0700

    update animations transittion

--- libs/mix.share/src/animations/slide.ts
+++ libs/mix.share/src/animations/slide.ts
@@ -3,7 +3,7 @@ import { animate, style, transition, trigger } from '@angular/animations';
 export const slideAnimation = trigger('enterAnimation', [
   transition(':enter', [
     style({ transform: 'translate(100%, 0)' }),
-    animate('200ms', style({ transform: 'translate(0, 0)' }))
+    animate('200ms ease', style({ transform: 'translate(0, 0)' }))
   ]),
   transition(':leave', [
     style({ opacity: 1 }),
