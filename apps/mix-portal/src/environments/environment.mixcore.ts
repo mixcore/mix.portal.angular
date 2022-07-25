c4bccb24bc7bd92a8867e9fb654e33eb1ba0cd1c
phong cao <phongcao3091998@gmail.com>
2022-07-25 15:47:43 +0700

    add build mixcore command line

---
+++ apps/mix-portal/src/environments/environment.mixcore.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  domainUrl: '/',
+  appRoot: '/portal',
+  baseUrl: '/api/v2/rest',
+  getThemeUrl:
+    'https://store.mixcore.org/api/v1/rest/en-us/post/client?pageSize=20&status=Published&orderBy=CreatedDateTime&direction=Desc&postType=theme'
+};
