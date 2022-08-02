2f89593018cfcbadec2c6beab4a0789ddd8912df
phong cao <phongcao3091998@gmail.com>
2022-08-02 16:59:50 +0700

    fix(mix.ui): fix build error

--- libs/mix.ui/src/index.ts
+++ libs/mix.ui/src/index.ts
@@ -1,4 +1,1 @@
-export { CodeEditorComponent } from './lib/code-editor/code-editor.component';
-export { InlineEditPlaceholderComponent } from './lib/inline-edit-placeholder/inline-edit-placeholder.component';
-export { RichTextEditorComponent } from './lib/rich-text-editor/rich-text-editor.component';
-export { SkeletonLoadingComponent } from './lib/skeleton-loading/skeleton-loading.component';
+export * from './lib';
---
+++ libs/mix.ui/src/lib/index.ts
@@ -0,0 +1,4 @@
+export { CodeEditorComponent } from './code-editor/code-editor.component';
+export { InlineEditPlaceholderComponent } from './inline-edit-placeholder/inline-edit-placeholder.component';
+export { RichTextEditorComponent } from './rich-text-editor/rich-text-editor.component';
+export { SkeletonLoadingComponent } from './skeleton-loading/skeleton-loading.component';
