5278198b8fe358b7e1afb1b12c77077a9ee26503
phong cao <phongcao3091998@gmail.com>
2023-08-11 01:07:58 +0700

    feat: add new table field design

--- apps/mix-cms/src/app/stores/database.store.ts
+++ apps/mix-cms/src/app/stores/database.store.ts
@@ -6,7 +6,7 @@ import { BaseCRUDStore } from './base-crud.store';
 @Injectable({ providedIn: 'root' })
 export class DatabaseStore extends BaseCRUDStore<MixDatabase> {
   public override requestFn = (request: PaginationRequestModel) =>
-    this.mixApi.databaseApi.gets({ ...request, columns: '' });
+    this.mixApi.databaseApi.gets({ ...request, columns: '', pageSize: 50 });
 
   public override mainUrl = '/' + CMS_ROUTES.portal.database.fullPath;
   public override requestName = 'database';
