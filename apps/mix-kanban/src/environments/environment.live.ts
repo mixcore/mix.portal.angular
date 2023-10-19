330e1a50fe1d8249380ed8c2629d07c71193d29f
develite98 <phongcao3091998@gmail.com>
2023-10-19 17:13:21 +0700

    feat() clean app

--- apps/mix-kanban/src/environments/environment.live.ts
+++ apps/mix-kanban/src/environments/environment.live.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  domainUrl: 'https://dev.mixcore.org',
-  stageDomainUrl: 'https://dev.mixcore.org',
+  domainUrl: '[[apiEndpoint]]',
+  stageDomainUrl: '[[apiEndpoint]]',
 };
